2960e54af9259fb4369ede1b0d232cd1a825cf7c
Aulia Rachmat Diapari <aulia.diapari@gmail.com>
2023-08-19 17:35:42 +0700

    commit

--- features/loginlogoutforgot.feature
+++ features/loginlogoutforgot.feature
@@ -1,16 +1,18 @@
+
+
 Feature: Shafiq Login
   Scenario: successfully Login with valid credentials
     Given user launch Chrome Browser
     When user open shafiq home page and go to login page
-    And user enter email "<email>" and password "<password>"
+    And user enter email "aulia.diapari@gmail.com" and password "Riverdale06!"
     And user click on login button
-    Then user must successfully login to the Dashboard page
+    Then user must successfully login to the dashboard page
   
   Scenario: successfully Logout from dashboard profile
-    When user perform Logout
-    Then user redirected to Home page
+    When user perform logout
+    Then user redirected to home page
 
   Scenario: successfully Forgot Password
     Then user navigate to forgot password page
-    When user enter email "<email>" and submit
+    When user enter email "<aulia.diapari@gmail.com>" and submit
     Then user will notify the validation message
--- features/steps/loginlogoutforgot.py
+++ features/steps/loginlogoutforgot.py
@@ -3,48 +3,51 @@ from locators import Locators
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import pdb
 import time
 
 
+
+
+# You can implement step definitions for undefined steps with these snippets:
+
 @given(u'user launch Chrome Browser')
-def launch_browser(self):
-    # Headed browser
-    self.driver = webdriver.Chrome()
-    self.driver.maximize_window()
+def step_impl(context):
+        # Headed browser
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
 
     # # Headless Browser
     # options = Options()
     # options.add_argument("--headless")  #
-    # self.driver = webdriver.Chrome(options=options)
+    # context.driver = webdriver.Chrome(options=options)
+
 
 ## Login Scneario
 @when(u'user open shafiq home page and go to login page')
-def homepage_and_loginpage(self):
-    self.driver.get(Locators.URL)
-    self.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE).click()
+def step_impl(context):
+    context.driver.get(Locators.URL)
+    context.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE).click()
 
 
-@when(u'user enter email "<aulia.diapari@gmail.com>" and password "<Riverdale06!>"')
-def step_impl(self):
-    self.driver.find_element(By.ID, Locators.LOGIN_FIELD_EMAIL).send_keys('aulia.diapari@gmail.com')
-    self.driver.find_element(By.ID, Locators.LOGIN_FIELD_KATA_SANDI).send_keys('Riverdale06!')
-    self.driver.switch_to.frame(0)
-    self.driver.find_element(By.CSS_SELECTOR, Locators.ALL_CAPTCHA).click()
+@when(u'user enter email "{email}" and password "{password}"')
+def step_impl(context, email, password):
+    context.driver.find_element(By.ID, Locators.LOGIN_FIELD_EMAIL).send_keys(email)
+    context.driver.find_element(By.ID, Locators.LOGIN_FIELD_KATA_SANDI).send_keys(password)
+    context.driver.switch_to.frame(0)
+    context.driver.find_element(By.CSS_SELECTOR, Locators.ALL_CAPTCHA).click()
     time.sleep(10)
 
 
 @when(u'user click on login button')
-def verify_captcha_and_login_button(self):
-    self.driver.switch_to.default_content()
+def step_impl(context):
+    context.driver.switch_to.default_content()
     time.sleep(2)
-    self.driver.find_element(By.CSS_SELECTOR, Locators.LOGIN_CLICK_BUTTON).click()
+    context.driver.find_element(By.CSS_SELECTOR, Locators.LOGIN_CLICK_BUTTON).click()
 
 
-
-@then(u'user must successfully login to the Dashboard page')
-def verify_dashboard(self):
-    sidebar_menu = self.driver.find_element
+@then(u'user must successfully login to the dashboard page')
+def step_impl(context):
+    sidebar_menu = context.driver.find_element
 
     DASHBOARD = sidebar_menu(By.CSS_SELECTOR, Locators.SIDEBAR_DASHBOARD)
     PORTOFOLIO = sidebar_menu(By.CSS_SELECTOR, Locators.SIDEBAR_PORTOFOLIO)
@@ -64,42 +67,45 @@ def verify_dashboard(self):
     assert LAPORAN.is_displayed(), "Laporan is not displayed"
     assert BANTUAN.is_displayed(), "Bantuan is not displayed"
 
+    print('assertion is passed')
 
-## Logout Scneario
-@when(u'user perform Logout')
-def step_impl(self):
-    self.driver.find_element(By.CSS_SELECTOR, Locators.PROFILE_DROPDOWN).click()
-    self.driver.find_element(By.LINK_TEXT, Locators.PROFILE_DROPDOWN_LOGOUT).click()
-    
 
-@then(u'user redirected to Home page')
-def step_impl(self):
-    verfiyInHomePage = self.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE)
+## Logout Scenario
+@when(u'user perform logout')
+def step_impl(context):
+    driver.find_element(By.CSS_SELECTOR, PROFILE_DROPDOWN).click()
+    driver.find_element(By.LINK_TEXT, PROFILE_DROPDOWN_LOGOUT).click()
+
+
+@then(u'user redirected to home page')
+def step_impl(context):
+    verfiyInHomePage = context.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE)
     assert verfiyInHomePage.is_displayed(), 'Not successfull redirect to homepage'
+
 
 
 ## Forgot Password Scenario
 @then(u'user navigate to forgot password page')
-def step_impl(self):
-    self.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE).click()
-    self.driver.find_element(By.XPATH, Locators.LUPA_KATA_SANDI_TXTLINK).click()
+def step_impl(context):
+    context.driver.find_element(By.XPATH, Locators.LOGIN_GO_TO_LOGIN_PAGE).click()
+    context.driver.find_element(By.XPATH, Locators.LUPA_KATA_SANDI_TXTLINK).click()
 
-    pageTitle = self.driver.find_element(By.XPATH, Locators.LUPA_KATA_SANDI_PGTITLE)
+    pageTitle = context.driver.find_element(By.XPATH, Locators.LUPA_KATA_SANDI_PGTITLE)
     assert pageTitle.is_displayed, 'PageTitle is not displayed'
 
 
-@when(u'user enter email "<aulia.diapari@gmail.com>" and submit')
-def step_impl(self):
-    self.driver.find_element(By.NAME, Locators.LUPA_KATA_SANDI_FIELD_EMAIL).send_keys('email@gmail.com')
-    self.driver.switch_to.frame(0)
-    self.driver.find_element(By.CSS_SELECTOR, Locators.ALL_CAPTCHA).click()
+@when(u'user enter email "{email}}" and submit')
+def step_impl(context):
+    context.driver.find_element(By.NAME, Locators.LUPA_KATA_SANDI_FIELD_EMAIL).send_keys(email)
+    context.driver.switch_to.frame(0)
+    context.driver.find_element(By.CSS_SELECTOR, Locators.ALL_CAPTCHA).click()
     time.sleep(10)
-    self.driver.switch_to.default_content()
-    self.driver.find_element(By.CSS_SELECTOR, Locators.LOGIN_CLICK_BUTTON).click()
+    context.driver.switch_to.default_content()
+    context.driver.find_element(By.CSS_SELECTOR, Locators.LOGIN_CLICK_BUTTON).click()
     time.sleep(2)
 
 
 @then(u'user will notify the validation message')
-def step_impl(self):
-    verifyMessage = self.driver.find_element(By.CSS_SELECTOR, Locators.LUPA_KATA_SANDI_ALLERT)
+def step_impl(context):
+    verifyMessage = context.driver.find_element(By.CSS_SELECTOR, Locators.LUPA_KATA_SANDI_ALLERT)
     assert verifyMessage.is_displayed()
